6dc6d5bf4e376108bc97fa393e7bdfcf244bda69
qqdog1 <qqdog1@yahoo.com.tw>
2019-12-10 22:18:27 +0800

    1. set analysis url

--- src/main/java/name/qd/analysis/controller/AnalysisController.java
+++ src/main/java/name/qd/analysis/controller/AnalysisController.java
@@ -4,10 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/analysis")
 public class AnalysisController {
 
 	@RequestMapping("/test")
 	public String test() {
 		return "123";
+	}
+	
+	@RequestMapping("")
+	public String getAnalysisResult() {
+		return "";
 	}
 }
