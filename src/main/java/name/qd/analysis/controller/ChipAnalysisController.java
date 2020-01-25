d081ff48ea75a13f4d4054cb8f08be737bc9374d
qqdog1 <qqdog1@yahoo.com.tw>
2020-01-26 00:06:14 +0800

    1. implement get best branch buy sell

--- src/main/java/name/qd/analysis/controller/ChipAnalysisController.java
+++ src/main/java/name/qd/analysis/controller/ChipAnalysisController.java
@@ -1,10 +1,46 @@
 package name.qd.analysis.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import name.qd.analysis.Constants.Action;
+import name.qd.analysis.service.ChipAnalysisService;
 
 @RestController
 @RequestMapping("/chip/analysis")
 public class ChipAnalysisController {
+	private static Logger logger = LoggerFactory.getLogger(ChipAnalysisController.class);
+	private static final String DAYS_OVER = "Days can't over than 20.";
+	
+	@Autowired
+	private ChipAnalysisService chipAnalysisService;
 
+	@RequestMapping(value = "/bestBranchBuy", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> bestBranchBuy(String market, int days, int branchs, long tradeCost) {
+		if(days > 20) {
+			logger.error(DAYS_OVER);
+			return new ResponseEntity<String>(DAYS_OVER, HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
+		String result = chipAnalysisService.bestBranchBuySell(market, days, branchs, tradeCost, Action.BUY);
+		return new ResponseEntity<String>(result, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/bestBranchSell", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> bestBranchSell(String market, int days, int branchs, long tradeCost) {
+		if(days > 20) {
+			logger.error(DAYS_OVER);
+			return new ResponseEntity<String>(DAYS_OVER, HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
+		String result = chipAnalysisService.bestBranchBuySell(market, days, branchs, tradeCost, Action.SELL);
+		return new ResponseEntity<String>(result, HttpStatus.OK);
+	}
 }
--- src/main/java/name/qd/analysis/service/ChipAnalysisService.java
+++ src/main/java/name/qd/analysis/service/ChipAnalysisService.java
@@ -1,5 +1,8 @@
 package name.qd.analysis.service;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
@@ -7,11 +10,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import name.qd.analysis.Constants.Action;
+import name.qd.analysis.Constants.Exchange;
+import name.qd.analysis.chip.ChipAnalyzers;
 import name.qd.analysis.chip.analyzer.ChipAnalyzerManager;
+import name.qd.analysis.dataSource.DataSource;
+import name.qd.analysis.dataSource.DataSourceFactory;
+import name.qd.analysis.utils.DateUtils;
+import name.qd.analysis.utils.JsonUtils;
 
 @Service
 public class ChipAnalysisService {
 	private static Logger logger = LoggerFactory.getLogger(ChipAnalysisService.class);
+	private DataSourceFactory dataSourceFactory = DataSourceFactory.getInstance();
+	private ObjectMapper objectMapper = JsonUtils.getObjectMapper();
 	
 	@Autowired
 	private InstanceService instanceService;
@@ -21,5 +37,29 @@ public class ChipAnalysisService {
 	@PostConstruct
 	private void init() {
 		chipAnalyzerManager = instanceService.getChipAnalyzerManager();
+	}
+	
+	public String bestBranchBuySell(String market, int days, int branchs, long tradeCost, Action side) {
+		Exchange exchange = Exchange.valueOf(market);
+		DataSource dataSource = dataSourceFactory.getDataSource(exchange, instanceService.getTWSEDataPath());
+		String[] inputs = {String.valueOf(branchs)};
+		List<Date> lstFromTo = DateUtils.getFromToByDays(instanceService.getTWSEDataPath(), market, days);
+		
+		List<List<String>> lst = chipAnalyzerManager.getAnalysisResult(dataSource, ChipAnalyzers.BEST_BRANCH_BUY_SELL, "", "", lstFromTo.get(0), lstFromTo.get(1), tradeCost, false, inputs);
+		ArrayNode node = objectMapper.createArrayNode();
+		
+		for(List<String> lstResult : lst) {
+			if(lstResult.get(2).equals(side.name())) {
+				ObjectNode objectNode = objectMapper.createObjectNode();
+				objectNode.put("branch", lstResult.get(0));
+				objectNode.put("product", lstResult.get(1));
+				objectNode.put("side", lstResult.get(2));
+				objectNode.put("price", lstResult.get(3));
+				objectNode.put("share", lstResult.get(4));
+				objectNode.put("volume", lstResult.get(5));
+				node.add(objectNode);
+			}
+		}
+		return node.toString();
 	}
 }
